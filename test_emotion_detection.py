538d57f837fc7ef4b8748d4393b4429935982375
Alejandro-CV12 <internet@cabrera.capital>
2026-06-27 17:50:39 -0400

    Agregar archivo de pruebas unitarias Tarea 5

---
+++ test_emotion_detection.py
@@ -0,0 +1,27 @@
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDetector(unittest.TestCase):
+    def test_emotion_detector(self):
+        # 1. Prueba para Alegría (joy)
+        result_1 = emotion_detector("I am glad this happened")
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
+        
+        # 2. Prueba para Enojo (anger)
+        result_2 = emotion_detector("I am really mad about this")
+        self.assertEqual(result_2['dominant_emotion'], 'anger')
+        
+        # 3. Prueba para Disgusto (disgust)
+        result_3 = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result_3['dominant_emotion'], 'disgust')
+        
+        # 4. Prueba para Tristeza (sadness)
+        result_4 = emotion_detector("I am so sad about this")
+        self.assertEqual(result_4['dominant_emotion'], 'sadness')
+        
+        # 5. Prueba para Miedo (fear)
+        result_5 = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result_5['dominant_emotion'], 'fear')
+
+if __name__ == '__main__':
+    unittest.main()
